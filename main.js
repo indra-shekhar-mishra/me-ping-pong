658274a0bd6127b3c1949f442779ed9614dcf27e
indra-shekhar-mishra <138847325+indra-shekhar-mishra@users.noreply.github.com>
2024-06-20 18:27:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var paddle2 =10,paddle1=10;
 
 var paddle1X = 10,paddle1Height = 600;
-var paddle2Y = 685,paddle2Height = 70;
+var paddle2Y = 685,paddle2Height = 5;
 
 var score1 = 0, score2 =0;
 var paddle1Y;
